b2181c57e6d1173394cd16e057d1376aa88a23e9
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2023-08-25 01:07:44 +0700

    feat(dobotics_test): Added ros2 talker and listener examples

---
+++ src/dobotics_test/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'dobotics_test'
+
+setup(
+    name                = package_name,
+    version             = '0.0.0',
+    packages            = find_packages(exclude=['test']),
+    data_files          = [
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires    = ['setuptools'],
+    zip_safe            = True,
+    maintainer          = 'dhonan',
+    maintainer_email    = 'dhonan.hibatullah@gmail.com',
+    description         = 'This package made for running several tests on the dobotics workspace.',
+    license             = 'None',
+    tests_require       = ['pytest'],
+    entry_points        = {
+        'console_scripts': [
+            'ros2_talker_example = dobotics_test.ros2_talker_example:main',
+            'ros2_listener_example = dobotics_test.ros2_listener_example:main'
+        ],
+    },
+)
