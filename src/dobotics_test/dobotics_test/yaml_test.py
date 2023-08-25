8b77be53f767299cec9cade850b5b4601c96eead
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2023-08-25 16:39:35 +0700

    Fix(dobotics_test): Quick fix on yaml_test.py

--- src/dobotics_test/dobotics_test/yaml_test.py
+++ src/dobotics_test/dobotics_test/yaml_test.py
@@ -7,7 +7,8 @@ def main(args=None):
     # Get the .yaml file
     with open(YAML_FILE_PATH, 'r') as file:
         yaml_file = yaml.load(file, Loader=yaml.FullLoader)
-    print(yaml_file)
+    for robot in yaml_file['robot']:
+        print(robot['name'])
 
 if __name__ == '__main__':
     main()
