a4d07d24c09af7db1b4c2babdc94f770f74cbb30
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2024-11-10 15:16:34 +0700

    Feat!: configured for multi-state control

--- src/dobotics_webots/dobotics_webots/supervisor.py
+++ src/dobotics_webots/dobotics_webots/supervisor.py
@@ -8,33 +8,38 @@ class SupervisorDriver:
 
     def init(self, webots_node, properties) -> None:
         self.parameter_json = json.load(open(get_package_share_directory('dobotics_webots') + f'/{properties['parameterFile']}', 'r'))
-        self.parameters     = self.parameter_json['params']
+        self.states         = self.parameter_json['states']
         self.sim_name       = self.parameter_json['name']
 
         self.robot: webots.Robot            = webots_node.robot
         self.timestep                       = int(self.robot.getBasicTimeStep())
         self.sim_time_ms                    = 0
         self.supervisor: webots.Supervisor  = webots.Supervisor()
-        self.sim_nodes: list[webots.Node]   = [self.supervisor.getFromDef(param['defName']) for param in self.parameters]
-        self.sim_node_num                   = len(self.sim_nodes)
         
         rclpy.init(args = None)
         self.node = rclpy.create_node(self.sim_name + '_SupervisorNode')
 
         self.reset_sub = self.node.create_subscription(
-            msg_type    = std_msgs.Empty,
+            msg_type    = std_msgs.String,
             topic       = f'{self.sim_name}/reset',
             callback    = self.resetCallback,
             qos_profile = 1000
         )
 
 
-    def resetCallback(self, msg: std_msgs.Empty) -> None:
-        for i in range(self.sim_node_num):
-            self.sim_nodes[i].getField('translation').setSFVec3f(self.parameters[i]['translation'])
-            self.sim_nodes[i].getField('rotation').setSFRotation(self.parameters[i]['rotation'])
+    def resetCallback(self, msg: std_msgs.String) -> None:
+        state_key = str(msg.data)
 
-        self.node.get_logger().info('The simulation is reset!')
+        if state_key in self.states:
+            for data in self.states[state_key]:
+                node = self.supervisor.getFromDef(data['defName'])
+                node.getField('translation').setSFVec3f(data['translation'])
+                node.getField('rotation').setSFRotation(data['rotation'])
+                
+            self.node.get_logger().info('The simulation is reset!')
+
+        else:
+            self.node.get_logger().error('Unknown state name.')
 
     
     def step(self) -> None:
